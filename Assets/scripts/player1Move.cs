b34d41a59a272a4396285ec9cbfbb0a1bb49c9ee
bluesun42_hash <82324500+bluesun42-hash@users.noreply.github.com>
2022-08-15 16:07:32 +0200

    joueur rotation
    le joueur regarde dans la direction dans laquelle il marche

--- Assets/scripts/player1Move.cs
+++ Assets/scripts/player1Move.cs
@@ -8,10 +8,16 @@ public class player1Move : MonoBehaviour
     public int speed;
     public Vector2 direction;
     public Rigidbody2D rb;
+    public float rotationSpeed;
 
     private void FixedUpdate()
     {
         rb.velocity = direction * speed * Time.fixedDeltaTime;
+        if(direction != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
+        }
     }
     public void onMove(InputAction.CallbackContext ctx)
     {
--- Assets/scripts/player2Move.cs
+++ Assets/scripts/player2Move.cs
@@ -8,10 +8,13 @@ public class player2Move : MonoBehaviour
     public int speed;
     public Vector2 direction;
     public Rigidbody2D rb;
+    public float rotationSpeed;
 
     private void FixedUpdate()
     {
         rb.velocity = direction * speed * Time.fixedDeltaTime;
+        Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
     }
     public void onMove(InputAction.CallbackContext ctx)
     {
