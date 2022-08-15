463dc7fbac1d0853138e9a1a29d178ba8293d4c0
bluesun42_hash <82324500+bluesun42-hash@users.noreply.github.com>
2022-08-15 15:49:45 +0200

    ajout des personnages
    déplacements de base et zones délimitées

---
+++ Assets/scripts/player1Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player1Move : MonoBehaviour
+{
+    public int speed;
+    public Vector2 direction;
+    public Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        rb.velocity = direction * speed * Time.fixedDeltaTime;
+    }
+    public void onMove(InputAction.CallbackContext ctx)
+    {
+        direction = ctx.ReadValue<Vector2>();
+    }
+}
