c9fd96f14be8e9375dc9f2bfe9247323ace6a679
bluesun42_hash <82324500+bluesun42-hash@users.noreply.github.com>
2022-08-17 15:22:08 +0200

    fichier des bugs à régler

---
+++ Assets/BUGS.cs
@@ -0,0 +1,3 @@
+//Listes des bugs à régler :
+// -  contact entre le p2 et la cible change parfois la direction de la cible
+// -  quand le p1 fait apparaître la cible et continue à bouger, il avance jusq'au collider suivant
