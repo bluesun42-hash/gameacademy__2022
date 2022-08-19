084e0d8d7b309502ba58f2dad78b0fa49c01dc36
IJJA3141 <109812161+IJJA3141@users.noreply.github.com>
2022-08-19 14:24:43 +0200

    Mousic

---
+++ Assets/Scripts/EnfantDoublage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnfantDoublage : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    Random rng = new Random;
+    public void KidSound()
+    {
+
+    }
+}
